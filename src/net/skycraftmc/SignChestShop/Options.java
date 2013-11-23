7022029427cf1b86f8fc94d29b9ac2ae8e1ce4bd
Technius <techniux@gmail.com>
2013-11-22 22:45:45 -0800

    Small config refactoring

--- src/net/skycraftmc/SignChestShop/Options.java
+++ src/net/skycraftmc/SignChestShop/Options.java
@@ -4,7 +4,7 @@ public class Options
 {
 	public static final String DEFAULT_BUY_MODE = "single";
 	public static final boolean DEFAULT_BUY_SHIFTCLICK = true;
-	public static final boolean DEFAULT_LOG_SHOP_CREATION = true;
+	public static final boolean DEFAULT_LOG_SHOP_CREATION = false;
 	public static final boolean DEFAULT_BUY_PERMS = false;
 	public static final boolean DEFAULT_BUY_PERMSID = false;
 	public static final boolean DEFAULT_SELL_PERMS = false;
