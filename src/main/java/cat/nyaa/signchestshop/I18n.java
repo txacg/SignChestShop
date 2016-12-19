1020c3fcfbcc2f922b650e25a19ccc53daec5a0f
cyilin <cyilin@users.noreply.github.com>
2016-12-19 19:40:32 +0800

    use new i18n api

--- src/main/java/cat/nyaa/signchestshop/I18n.java
+++ src/main/java/cat/nyaa/signchestshop/I18n.java
@@ -1,18 +1,32 @@
 package cat.nyaa.signchestshop;
 
 import cat.nyaa.utils.Internationalization;
+import co.technius.signchestshop.SignChestShopPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class I18n extends Internationalization {
     public static I18n instance = null;
+    private final SignChestShopPlugin plugin;
+    private String lang = null;
 
-    public I18n(JavaPlugin plugin, String lang) {
-        super(plugin);
+    public I18n(SignChestShopPlugin plugin, String lang) {
         instance = this;
-        load(lang);
+        this.plugin = plugin;
+        this.lang = lang;
+        load();
     }
 
     public static String _(String key, Object... args) {
         return instance.get(key, args);
+    }
+
+    @Override
+    protected JavaPlugin getPlugin() {
+        return plugin;
+    }
+
+    @Override
+    protected String getLanguage() {
+        return lang;
     }
 }
